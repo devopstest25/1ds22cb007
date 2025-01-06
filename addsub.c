832b35b9be30e8b38ecb8e16ac246790a6cbf348
devopstest25 <devopscsbs@gmail.com>
2025-01-06 10:18:00 +0530

    First commit

---
+++ addsub.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter any 2 numbers - ");
+    scanf("%d %d",&a,&b);
+    printf("Sum = %d",a+b);
+    printf("Diff = %d",a-b);
+    return 0;
+}
